53738d8d4e5ef0f9a72f6afd6a137b89a6d29f3d
fiyn <alienafis@gmail.com>
2024-02-01 00:05:57 +0800

    Worked on TC04 - Dashboard, Add Test Suites to 02 - Feed

---
+++ Empxtrack_Nafis/Test Suites/TS04 - Dashboard/TS_004_Feed.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_004_Feed</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>192ff78a-fd07-4b0b-93fa-49a2fd9c283d</testSuiteGuid>
+   <testCaseLink>
+      <guid>b9efe636-6727-46f2-b8b1-6eae77bbd838</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_017_AddComment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>eb26a32c-9ffb-4172-80d6-0622e68e4110</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2f4b88d2-a8f3-44fe-92f2-01590b5c337b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_018_ExpandCommentSection</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0e259485-d937-4b61-bd95-271d386ef1f4</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7dc5be4b-2562-45d8-beb4-ef73603e95f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_019_EmojiComment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>835cc01b-65ac-4a57-9dce-e5e2284a5003</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_020_LikeUserFeed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>56b3a29d-d5f2-4f60-b282-c83414dc547d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/02 - Negative/TC_021_EmptyComment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>1d46d820-f65d-49b9-848c-2ad8a113f9ab</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4da21ad-7193-4543-a6b7-f8a262a50328</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_022_ViewProfile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9dd0147-7d99-4822-af77-218ebab48f9c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_023_ViewProfileFullPage</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bc3e1711-4c9d-44e1-a2e5-292ce9e655b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/01 - Positive/TC_024_SeeAll</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>89a018b3-cca8-4a65-ad88-f7793a7a3328</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - Dashboard/02 - Feed/02 - Negative/TC_025_AddNewFeed</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
