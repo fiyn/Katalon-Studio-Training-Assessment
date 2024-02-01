a3d6de2d4e8fc16c0dc8dada919be4c8c0b3060d
fiyn <alienafis@gmail.com>
2024-02-01 13:29:25 +0800

    Worked on TC05 - Self Services, 01 - Profile (TC_040 - TC_043)

--- Empxtrack_Nafis/Scripts/TC05 - Self Services/01 - Profile/01 - Positive/TC_041_ViewProfileFullPage/Script1706757530811.groovy
+++ Empxtrack_Nafis/Scripts/TC05 - Self Services/01 - Profile/01 - Positive/TC_041_ViewProfileFullPage/Script1706757530811.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('TC05 - Self Services/01 - Profile/TC_040_ViewProfile'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('TC05 - Self Services/01 - Profile/01 - Positive/TC_040_ViewProfile'), [:], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.waitForElementVisible(findTestObject('TC05 - Self Services/01 - Profile/TC_041_ViewProfileFullPage/Page_Home page/a_View in full page'), 
     2)
